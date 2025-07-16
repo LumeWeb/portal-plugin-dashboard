9befa85864c27b706d5facd56283e5469fceeff5
Derrick Hammer <derrick@derrickhammer.com>
2025-07-16 10:06:17 -0400

    fix: migrate sql migrations to goose syntax

--- internal/db/migrations/sqlite/20250604064114_init.sql
+++ internal/db/migrations/sqlite/20250604064114_init.sql
@@ -1,4 +1,5 @@
--- migrate:up
+-- +goose Up
+-- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS api_keys (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -9,6 +10,9 @@ CREATE TABLE IF NOT EXISTS api_keys (
     user_id INTEGER NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
     expires DATETIME NULL
 );
+-- +goose StatementEnd
 
--- migrate:down
+-- +goose Down
+-- +goose StatementBegin
 DROP TABLE api_keys;
+-- +goose StatementEnd
